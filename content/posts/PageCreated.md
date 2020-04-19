25cec226218c1fd38080b70eec6e6288fb8d1198
Kun Chin <KunChin@users.noreply.github.com>
2020-04-20 00:54:06 +0800

    Fix diary

--- content/posts/PageCreated.md
+++ content/posts/PageCreated.md
@@ -1,5 +1,5 @@
 ---
-title: "PageCreated"
+title: "Page Created！"
 date: 2020-04-16T17:17:32+08:00
 ---
 这个博客在今天上线了。
---
+++ docs/cn.search.min.8c23706299aee9ea3b07aa1f688c8933c5b6f834f08fc97fd473c4ed532bc3b0.js
@@ -0,0 +1,9 @@
+'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);document.addEventListener('keypress',focusSearchFieldOnKeyPress);function focusSearchFieldOnKeyPress(event){if(input===document.activeElement){return;}
+const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
+input.focus();event.preventDefault();}
+function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/Moon.github.io/flexsearch.min.js');loadScript('/Moon.github.io/cn.search-data.min.17e77aad86d2553fcc4251568743b3967cec78fd8adb3b93b44d204ec2d79b17.js',function(){input.required=false;search();});}
+function search(){while(results.firstChild){results.removeChild(results.firstChild);}
+if(!input.value){return;}
+const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
+function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
